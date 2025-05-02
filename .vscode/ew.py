8b5eff03280d8858d9388dc503fec9eadef72d69
ojayballer <omojiremurewa@gmail.com>
2025-05-02 23:49:40 +0100

    Update ew.py

--- .vscode/ew.py
+++ .vscode/ew.py
@@ -1,13 +1,1 @@
-from tkinter import *
-
-root=Tk()
-#creating label widget
-myLabel1=Label(root,text="Hello world!")
-myLabel2=Label(root,text="my name is omojire!")
-
-#Shoving it onto the screen
-myLabel1.grid(row=0,column=1)
-myLabel2.grid(row=1,column=1)
-#event loop
-root.mainloop()
 
