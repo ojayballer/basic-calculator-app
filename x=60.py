6d14111fba603881f86240535d409a6333235ab1
ojayballer <omojiremurewa@gmail.com>
2025-05-02 23:53:33 +0100

    Update x=60.py

--- x=60.py
+++ x=60.py
@@ -1,17 +1,1 @@
-class Vehicle():
-    def __init__(self):
-     def move(self):
-        print("vehicle is moving")
-class Car(Vehicle):
-    def __init__(self):
-        super().__init__()
-        
-    def move(self):
-        print("car is moving")
-class electricCar(Car):
-     def __init__(self):
-         super().__init__()
-     def move(self):
-         print("electricCar is moving")
-car=Car()
-car.move()
+
